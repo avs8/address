f259b8c92c91ec46d12c42dc27137c21349899f2
ajita singh <ajitavsingh_8@yahoo.com>
2017-07-17 17:35:16 -0400

    added

--- address/settings.py
+++ address/settings.py
@@ -101,14 +101,7 @@ DATABASES = {
     }
 }
 
-import dj_database_url
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
 
-# Update database configuration with $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
 
 AUTHENTICATION_BACKENDS = ('django.contrib.auth.backends.ModelBackend',)
 
